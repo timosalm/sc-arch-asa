535ac06db8599a4e0b7fee2af3892e28c9e2869d
Timo Salm <52704922+timosalm@users.noreply.github.com>
2024-02-23 08:15:45 +0100

    Improvements

--- product-service/src/main/java/com/example/productservice/product/ProductApplicationService.java
+++ product-service/src/main/java/com/example/productservice/product/ProductApplicationService.java
@@ -4,10 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@RefreshScope
 @Service
 public class ProductApplicationService {
 
